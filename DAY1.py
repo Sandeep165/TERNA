7f42f2d0df2ab683943c4f6e411a4f62911086b3
Sandeep M Waghmare <sandeepwaghmare1999@gmail.com>
2021-12-16 10:22:05 +0530

    DAY1

---
+++ DAY1.py
@@ -0,0 +1,144 @@
+'''
+
+
+Github :- https://github.com/
+
+
+
+Overall flow:-
+Basic + Adv
+
+Basics:- 
+data types
+if else
+loops
+
+Test1/2/3:
+revise
+
+
+func
+oops
+
+Test1/2
+
+
+Adv:- DSA
+Stack
+Queue
+Linkedlist
+Trees
+Sorting
+DBMS
+Mini project
+
+'''
+
+
+#Variable Declaration
+'''
+1) should not start with keywords
+2) no spcl char at the start
+3)should not start with spcl char (@#$%)
+
+'''
+# name = "Sandeep"
+# age = 15
+# list1 = "mumbai"
+# list1 = 'mumbai'
+# list1 = '''mumbai'''
+# num = 10
+# num2 = 10.0
+
+# # My age is 15
+
+# print("My age is ",age )
+# # My name is Sandeep
+# print("My name is ", name)
+
+name = "Harry"
+age = 17
+city = "Mumbai"
+
+
+# My name is Harry. I am 17yrs old. I live in Mumbai. 
+# print("My name is ", name,".", "I am ",age, "yrs old", "I live in ", city)
+#String formatting
+# print(f"My name is {name}. I am {age}yrs old. I live in {city}." )
+
+# num1 = 10
+# num2 = 12
+
+# print("Value of num1 is ", num1, "Value of num2 is ", num2)
+
+#user input
+# num1 = int(input("Enter the number :- "))
+# name_1 = input("Enter your name:- ")
+
+# print(num1)
+# print(name_1)
+
+# f_name, l_name, clg, brach, roll_no
+# first name is fname. last name is lname, clg name is xyz, branch is xyz, rollno is abc
+
+# l_name =input("Enter your l_name:")
+# f_name =input("Enter your f_name:")
+# branch_name =input("Enter your branch_name:")
+# clg =input("Enter your clg_name:")
+# roll_no =int(input("Enter your roll_no:"))
+
+
+# print(f"first name is {f_name}. last name is {l_name}. clg name is {clg}.rollno is {roll_no} ")
+
+#if else
+'''
+
+n1 = 100  n2=200
+if (condition):
+    print()
+else:
+    print()
+
+'''
+num1 = 112
+num2 = 254
+
+# if (num1>num2)  :
+#     print("num1 is greater")
+# else:
+#     print("num2 is greater")
+
+#age = xyz   18< you are under age   18> you are adult
+
+
+age = int(input("enter your age:- "))
+if (age>18):
+    print("i am adult")
+else:
+    print("you are under age")
+
+'''
+
+statement1 or statement2 -> T/F
+tea 
+coffee
+tea and coffee
+
+(either of the condtion should be true -> True)
+T or T -> true
+T or F -> true
+F or T -> true
+F or F -> False
+
+coffee
+tea
+coffe and tea
+
+(either of the condtion is false -> False)
+T and T -> true
+T and F -> False
+F and T -> False
+F and F -> False
+
+'''
+
